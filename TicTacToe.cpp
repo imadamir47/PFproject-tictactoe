ad7858ab5290214055a3e85d063bb59d6e21217b
imadamir47 <imadamir47@gmail.com>
2026-06-01 19:47:45 +0500

    Add files via upload

---
+++ TicTacToe.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char board[3][3]=
+    {
+        {' ',' ',' '},
+        {' ',' ',' '},
+        {' ',' ',' '}
+    };
+
+    const char playerX='X';
+    const char playerO='O';
+    char winner =' ';
+    char currentPlayer=playerX;
+    int r=-1;
+    int c=-1;
+    for (int i=0;i<9;i++)
+    {
+        cout<<"   |   |   "<<endl;
+        cout<<" "<<board[0][0]<<" | "<<board[0][1]<<" | "<<board[0][2]<<endl;
+        cout<<"___|___|___"<<endl;
+        cout<<"   |   |   "<<endl;
+        cout<<" "<<board[1][0]<<" | "<<board[1][1]<<" | "<<board[1][2]<<endl;
+        cout<<"___|___|___"<<endl;
+        cout<<"   |   |   "<<endl;
+        cout<<" "<<board[2][0]<<" | "<<board[2][1]<<" | "<<board[2][2]<<endl; 
+        cout<<"   |   |   "<<endl;
+
+        if(winner!=' ')
+        {
+            break;
+        }
+
+        cout<<"Current player is "<<currentPlayer<<endl;
+        while(true)
+        {
+            cout<<"Enter the index of the box you want to fill (0-2)=";
+            cin>>r>>c;
+            if(r<0||r>2||c<0||c>2)
+            {
+                cout<<"Invalid input try Again."<<endl;
+            }
+            else if (board[r][c]!=' ')
+            {
+                cout<<"Tile is full try again."<<endl;
+            }
+            else
+            {
+                break;
+            }
+            r=-1;
+            c=-1;
+            cin.clear();
+            cin.ignore(10000,'\n');
+        }
+        board[r][c]=currentPlayer;
+        if(currentPlayer==playerX)
+        {
+            currentPlayer=playerO;
+        }
+        else
+        {
+            currentPlayer=playerX;
+        }
+        for(int r=0;r<3;r++)
+        {
+            if(board[r][0]!=' '&& board[r][0]==board[r][1]&& board[r][0]==board[r][2])
+            {
+                winner=board[r][0];
+                break;
+            }
+        }
+        for(int c=0;c<=3;c++)
+        {
+            if(board[0][c]!=' '&& board[0][c]==board[1][c]&& board[0][c]==board[2][c])
+            {
+                winner=board[c][0];
+                break;
+            }
+        }
+        if(board[0][0]!=' '&& board[0][0]==board[1][1]&& board[1][1]==board[2][2])
+        {
+            winner=board[0][0];
+        }
+        else if(board[0][2]!=0&& board[0][2]==board[1][1]&& board[1][1]==board[2][0])
+        {
+            winner=board[0][2];
+        }
+    }
+    if(winner!=' ')
+    {
+        cout<<"Yayyyy player "<<winner<<" won the game"<<endl;
+    }
+    else
+    {
+        cout<<"Tie!"<<endl;
+    }
+}
